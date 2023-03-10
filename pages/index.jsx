7460a895581697cba02a947732c657be3afe481a
Dan <d.lapp14@gmail.com>
2023-03-09 23:34:32 -0500

    album release

--- pages/index.jsx
+++ pages/index.jsx
@@ -24,24 +24,46 @@ function Home({ latestPost }) {
         <title>{pageTitle}</title>
         <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
       </Head>
-      <section className="section--dark-translucent">
-        <h1 className="brand-header">
-          Welcome to the official Wasting Paper band website
-        </h1>
-      </section>
-      <section className="section--dark text-center">
-        <p>
-          We are a Rock band from Hamilton, Ontario. We try to make loud noises
-          that people like to hear.
-        </p>
-        <p className="side-note">All rights reserved. Patent pending.</p>
-      </section>
 
       <section className="section--dark-translucent">
         <div className="panel panel-flex">
-          <p className="panel-flex--content-main paragraph-focus">
-            New album <strong>Bits &amp; Parts</strong> out March&nbsp;10th.
-          </p>
+          <div className="panel-flex--content-main">
+            <div className="section__content translucent-content">
+              <p className="paragraph-focus">
+                New album <strong>Bits &amp; Parts</strong> out now!
+              </p>
+              <p>Listen wherever you stream</p>
+              <div className="music-locations" style={{ marginTop: "32px" }}>
+                <a
+                  href="https://music.apple.com/us/album/wrong-to-save-me/1671198941?i=1671198942"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="icon-social music__small">
+                    <Image src={appleMusicIcon} alt="Listen on Apple Music" />
+                  </div>
+                </a>
+                <a
+                  href="https://open.spotify.com/track/04pTnjhboOZHsbyJFBjlNn?si=zqcTX_o0R-aCKt2eCnER6w"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="icon-social music__small">
+                    <Image src={spotifyIcon} alt="Listen on Spotify" />
+                  </div>
+                </a>
+                <a
+                  href="https://www.youtube.com/watch?v=5SD1NsUKHVA"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="icon-social music__small">
+                    <Image src={youtubeIcon} alt="Listen on YouTube" />
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
           <div className="panel__image" style={albumCoverStyle}>
             <Image
               src={bitsAndPartsCover}
@@ -53,47 +75,36 @@ function Home({ latestPost }) {
 
       <section className="section--dark">
         <div className="panel panel-flex">
-          <div className="panel__image" style={albumCoverStyle}>
-            <Image
-              src={wtsmCover}
-              alt="Wrong to save me single cover by Wasting Paper"
-            />
-          </div>
           <div className="panel-flex--content-main">
             <p className="paragraph-focus">
-              Single <strong>Wrong to Save Me</strong> out now!
+              Check out the <strong>Wrong to Save Me</strong> music video
             </p>
-            <div className="music-locations" style={{ marginTop: "32px" }}>
-              <a
-                href="https://music.apple.com/us/album/wrong-to-save-me/1671198941?i=1671198942"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="icon-social music__small">
-                  <Image src={appleMusicIcon} alt="Listen on Apple Music" />
-                </div>
-              </a>
-              <a
-                href="https://open.spotify.com/track/04pTnjhboOZHsbyJFBjlNn?si=zqcTX_o0R-aCKt2eCnER6w"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="icon-social music__small">
-                  <Image src={spotifyIcon} alt="Listen on Spotify" />
-                </div>
-              </a>
-              <a
-                href="https://www.youtube.com/watch?v=5SD1NsUKHVA"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="icon-social music__small">
-                  <Image src={youtubeIcon} alt="Listen on YouTube" />
-                </div>
-              </a>
+            <div className="video-embed">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/At2T6yh2afc"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              />
             </div>
           </div>
         </div>
+      </section>
+
+      <section className="section--dark-translucent">
+        <h1 className="brand-header">
+          Welcome to the official Wasting Paper band website
+        </h1>
+      </section>
+      <section className="section--dark text-center">
+        <p>
+          We are a Rock band from Hamilton, Ontario. We try to make loud noises
+          that people like to hear.
+        </p>
+        <p className="side-note">All rights reserved. Patent pending.</p>
       </section>
 
       {/* <section className="section--dark">
